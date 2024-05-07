b434062b8f93abf556d58c679b2f436920fc3667
Vipul Agnihotri <vkagnihotri1341@gmail.com>
2024-05-07 16:46:34 +0530

    Initial commit

---
+++ Area_Of_Rectangle.java
@@ -0,0 +1,11 @@
+public class Area_Of_Rectangle
+      {
+          public static void main (String [] args)
+               {
+                   int recLength = Integer.parseInt(args[0]);
+                   int recBreadth = Integer.parseInt(args[1]);
+             System.out.println("Area Of Rectangle = "+(recLength*recBreadth));
+
+               }
+          
+      }
