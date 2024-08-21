459f67d1f9d80a810986febba99db67547f57ee7
Vipul Agnihotri <156226082+vkagni1341@users.noreply.github.com>
2024-08-21 17:44:40 +0530

    Tax_Calculate

---
+++ Tax_Calculate.java
@@ -0,0 +1,11 @@
+public class Tax_Calculate
+{
+   public static void main(String[]args)
+{
+  double incomeAmount = 990000;
+  double taxRate = 4.9f;
+   double TAX_AMOUNT = 990000*4.9/100;
+     System.out.println("TOTAl TAX_AMOUNT "+TAX_AMOUNT);
+}
+}
+
